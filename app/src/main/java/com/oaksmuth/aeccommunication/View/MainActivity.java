17f35b1929070ed909de24647b76ff88b970f507
Teerapord Lin <ropcj@hotmail.com>
2017-11-08 21:19:41 +0700

    swipe ok but have bug in nav press

--- app/src/main/java/com/oaksmuth/aeccommunication/View/MainActivity.java
+++ app/src/main/java/com/oaksmuth/aeccommunication/View/MainActivity.java
@@ -6,10 +6,14 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 
+import com.oaksmuth.aeccommunication.Controller.ViewPagerAdapter;
 import com.oaksmuth.aeccommunication.Model.Topic;
 import com.oaksmuth.aeccommunication.R;
 
@@ -20,6 +24,15 @@ public class MainActivity extends AppCompatActivity implements PlayFragment.OnTo
 
     private String backState;
     private short screenState;
+
+    BottomNavigationView bottomNavigationView;
+
+    private ViewPager viewPager;
+
+    PlayFragment playFragment;
+    TalkFragment talkFragment;
+    TeachFragment teachFragment;
+    MenuItem prevMenuItem;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -60,14 +73,65 @@ public class MainActivity extends AppCompatActivity implements PlayFragment.OnTo
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        //ini viewPager
+        viewPager = (ViewPager)findViewById(R.id.content);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                }
+                else
+                {
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                Log.d("page", "onPageSelected: "+position);
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = bottomNavigationView.getMenu().getItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        //swipe
+        viewPager.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+            }
+        });
+
+        setupViewPager(viewPager);
+
         //To place content with Play
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         PlayFragment playFragment = new PlayFragment();
         backState = playFragment.getClass().getName();
         fragmentTransaction.replace(R.id.content, playFragment).commit();
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        playFragment=new PlayFragment();
+        talkFragment=new TalkFragment();
+        teachFragment=new TeachFragment();
+        adapter.addFragment(playFragment);
+        adapter.addFragment(talkFragment);
+        adapter.addFragment(teachFragment);
+        viewPager.setAdapter(adapter);
     }
 
     @Override
